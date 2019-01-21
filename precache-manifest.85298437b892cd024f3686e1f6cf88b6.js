a0e2b2e32ddaf60ac61def1ec405085648d25009
Dylan Clarry <dylan.imail@gmail.com>
2019-01-21 01:42:30 -0500

    Add files via upload

---
+++ precache-manifest.85298437b892cd024f3686e1f6cf88b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c26ea1412e3c8670c842",
+    "url": "/toronto-waste-lookup/static/css/main.b5832ca9.chunk.css"
+  },
+  {
+    "revision": "c26ea1412e3c8670c842",
+    "url": "/toronto-waste-lookup/static/js/main.c26ea141.chunk.js"
+  },
+  {
+    "revision": "fafbd742bc93bd3a6c1a",
+    "url": "/toronto-waste-lookup/static/js/1.fafbd742.chunk.js"
+  },
+  {
+    "revision": "b8bf0a16a6b4be6bcb34",
+    "url": "/toronto-waste-lookup/static/js/runtime~main.b8bf0a16.js"
+  },
+  {
+    "revision": "da3101ab3a07a66411e540db311e5192",
+    "url": "/toronto-waste-lookup/index.html"
+  }
+];
